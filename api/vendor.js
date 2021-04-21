e9459e4ef64c14bba3923d6902ad10bb83101163
verbal-noun <kaifahsan420@gmail.com>
2021-04-21 22:11:47 +1000

    Added auth token for vendor

--- api/vendor.js
+++ api/vendor.js
@@ -1,101 +1,141 @@
 // Check if we are in production or development
 if (process.env.NODE_ENV !== "production") {
-    require("dotenv").config();
+  require("dotenv").config();
 }
 
 const express = require("express");
 const router = express.Router();
 const schema = require("./schemas");
+const passport = require("passport");
+// Dummy list to hold users
+users = [
+  {
+    id: 1,
+    name: "jack",
+    token: "123456789",
+    displayName: "Jack",
+    emails: [{ value: "jack@example.com" }],
+  },
+  {
+    id: 2,
+    name: "jill",
+    token: "abcdefghi",
+    displayName: "Jill",
+    emails: [{ value: "jill@example.com" }],
+  },
+];
+
+// Load auth-token config
+const initialisePassportBearer = require("../config/passport-token-config");
+
+initialisePassportBearer(passport, (token) =>
+  // TODO: Finds the user based on token from the database
+  users.find((user) => user.token === token)
+);
 
 // --------------------------------------------------------------- STATUS -----------------------------------------------------
 // POST request for opening for business
-router.post("/open", (req, res) => {
+router.post(
+  "/open",
+  passport.authenticate("bearer", { session: false }),
+  (req, res) => {
     let vendorID = req.user.id;
     var query = schema.Vendor.findByIdAndUpdate(vendorID, {
-        open: true, 
-        address: req.body.address, 
-        position: req.body.location
+      open: true,
+      address: req.body.address,
+      position: req.body.location,
     });
     query.exec((err, updated) => {
-        if(err) {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        } else {
-            res.send(updated);
-        }
+      if (err) {
+        console.log(err.message);
+        res.status(500).send(err.message);
+      } else {
+        res.send(updated);
+      }
     });
-});
+  }
+);
 
 // POST request for closing shop
-router.post("/close", (req, res) => {
+router.post(
+  "/close",
+  passport.authenticate("bearer", { session: false }),
+  (req, res) => {
     let vendorID = req.user.id;
-    var query = schema.Vendor.findByIdAndUpdate(vendorID, {open: false});
+    var query = schema.Vendor.findByIdAndUpdate(vendorID, { open: false });
     query.exec((err, updated) => {
-        if(err) {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        } else {
-            res.send(updated);
-        }
+      if (err) {
+        console.log(err.message);
+        res.status(500).send(err.message);
+      } else {
+        res.send(updated);
+      }
     });
-});
+  }
+);
 
 // POST request for relocating van to new location
-router.post("/relocate", (req, res) => {
+router.post(
+  "/relocate",
+  passport.authenticate("bearer", { session: false }),
+  (req, res) => {
     let vendorID = req.user.id;
     var query = schema.Vendor.findByIdAndUpdate(vendorID, {
-        address: req.body.address,
-        position: req.body.location
+      address: req.body.address,
+      position: req.body.location,
     });
     query.exec((err, updated) => {
-        if(err) {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        } else {
-            res.send(updated);
-        }
+      if (err) {
+        console.log(err.message);
+        res.status(500).send(err.message);
+      } else {
+        res.send(updated);
+      }
     });
-});
+  }
+);
 
 // --------------------------------------------------------------- ORDERS -----------------------------------------------------
 // GET request for fetching unfulfilled orders
 router.get("/orders", (req, res) => {
-    let vendorID = req.user.id;
-    var query = schema.Order.find({vendor: vendorID, status: "Preparing"});
-    query.exec((err, orders) => {
-        if(err) {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        } else {
-            res.send(orders);
-        }
-    });
+  let vendorID = req.user.id;
+  var query = schema.Order.find({ vendor: vendorID, status: "Preparing" });
+  query.exec((err, orders) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      res.send(orders);
+    }
+  });
 });
 
 // GET request for fetching order details
 router.get("/order/:orderID", (req, res) => {
-    var query = schema.Order.findById(req.params.orderID);
-    query.exec((err, orders) => {
-        if(err) {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        } else {
-            res.send(orders);
-        }
-    });
+  var query = schema.Order.findById(req.params.orderID);
+  query.exec((err, orders) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      res.send(orders);
+    }
+  });
 });
 
 // POST request for fulfilling an order
 router.post("/fulfillOrder", (req, res) => {
-    var query = schema.Order.findById(req.body.order, {status: "Ready for pickup"});
-    query.exec((err, updated) => {
-        if(err) {
-            console.log(err.message);
-            res.status(500).send(err.message);
-        } else {
-            res.send(updated);
-        }
-    });
+  var query = schema.Order.findById(req.body.order, {
+    status: "Ready for pickup",
+  });
+  query.exec((err, updated) => {
+    if (err) {
+      console.log(err.message);
+      res.status(500).send(err.message);
+    } else {
+      res.send(updated);
+    }
+  });
 });
 
 module.exports = router;
