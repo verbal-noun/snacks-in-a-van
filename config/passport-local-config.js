a29a3f63ab508959be643328e572d724c79e8694
Keith Leonardo <kleonardo52@gmail.com>
2021-04-22 17:18:03 +1000

    Update passport local config to allow async getUser functions

--- config/passport-local-config.js
+++ config/passport-local-config.js
@@ -1,9 +1,9 @@
 const LocalStrategy = require("passport-local").Strategy;
 const bcrypt = require("bcrypt");
 
-function initialize(passport, getUserByEmail, getUserById) {
+async function initialize(passport, getUserByEmail, getUserById) {
   const authenticateUser = async (email, password, done) => {
-    const user = getUserByEmail(email);
+    const user = await getUserByEmail(email);
 
     if (user == null) {
       return done(null, false, { message: "No user with that email" });
@@ -26,8 +26,8 @@ function initialize(passport, getUserByEmail, getUserById) {
 
   passport.use(new LocalStrategy({ usernameField: "email" }, authenticateUser));
   passport.serializeUser((user, done) => done(null, user.id));
-  passport.deserializeUser((id, done) => {
-    return done(null, getUserById(id));
+  passport.deserializeUser(async (id, done) => {
+    return done(null, await getUserById(id));
   });
 }
 
